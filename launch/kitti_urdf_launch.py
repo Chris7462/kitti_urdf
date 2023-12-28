57dec612f71a4174a954782e89a832554169e5ce
chris7462 <chris7462@gmail.com>
2023-12-28 14:25:23 -0500

    Fix coding style

--- launch/kitti_urdf_launch.py
+++ launch/kitti_urdf_launch.py
@@ -1,21 +1,24 @@
+from os.path import join
+
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-from os.path import join
+
 import xacro
 
 
 def generate_launch_description():
     # Use xacro to process the file
-    xacro_file = join(get_package_share_directory("kitti_urdf"), "description", "kitti_urdf.xacro")
+    xacro_file = join(get_package_share_directory('kitti_urdf'), 'description', 'kitti_urdf.xacro')
     kitti_description = xacro.process_file(xacro_file).toxml()
 
     # Configure the node
     robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
         parameters=[{
-            "robot_description": kitti_description
+            'robot_description': kitti_description
         }]
     )
 
