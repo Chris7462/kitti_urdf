2787deb5c606be237ffeab2576d88094fde87741
chris7462 <chris7462@gmail.com>
2026-06-27 22:29:15 -0400

    bump to lyrical

--- launch/kitti_urdf_rviz_launch.py
+++ launch/kitti_urdf_rviz_launch.py
@@ -5,17 +5,15 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('kitti_urdf')
+
     robot_state_publisher_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('kitti_urdf'), 'launch', 'kitti_urdf_launch.py'
-            ])
+            join(pkg_share, 'launch', 'kitti_urdf_launch.py')
         ])
     )
 
@@ -23,8 +21,7 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', join(get_package_share_directory('kitti_urdf'),
-                              'rviz', 'kitti_urdf.rviz')]
+        arguments=['-d', join(pkg_share, 'rviz', 'kitti_urdf.rviz')]
     )
 
     return LaunchDescription([
